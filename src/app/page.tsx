adfe5d7831035b8f53db78bf022053c5aad5f396
waribikimeshi <aee973465@equ.bbiq.jp>
2024-12-20 14:47:12 +0900

    作り直し

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <div>http://localhost:3000/</div>
+      <div>エントリポイント</div>
+      <div>app/page.tsx</div>
+    </>
+  );
+}

237892fdb9164b60224ee39227ffd51cd1a8e0f9
waribikimeshi <aee973465@equ.bbiq.jp>
2024-12-29 10:46:09 +0900

    リンクid

--- src/app/components/molecules/AuthenticationListLine.tsx
+++ src/app/components/molecules/AuthenticationListLine.tsx
@@ -1,4 +1,5 @@
 import { IAuthentication } from '@/app/authentication/types'
+import Link from 'next/link';
 import React from 'react'
 
 interface IAuthenticationProps{
@@ -9,9 +10,18 @@ const AuthenticationListLine = ({authentication}:IAuthenticationProps) => {
   return (
             <li key={authentication.id}>
                 <span>{authentication.mailAddress}</span>
-                <div><button>参照</button></div>
-                <div><button>編集</button></div>
-                <div><button>削除</button></div>    
+
+                <Link href={`/authentication/read/${authentication.id}`}　passHref>
+                    <button>参照</button>
+                </Link>
+
+                <Link href={`/authentication/update/${authentication.id}`}　passHref>
+                    <button>更新</button>
+                </Link>
+
+                <Link href={`/authentication/delete/${authentication.id}`}　passHref>
+                    <button>削除</button>
+                </Link>
             </li>
 )
 }

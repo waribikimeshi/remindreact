f118b165044a993b78f2f5f6364998f837746c69
waribikimeshi <aee973465@equ.bbiq.jp>
2024-12-29 14:06:26 +0900

    async漏れ

---
+++ src/app/authentication/[id]/update/page.tsx
@@ -0,0 +1,28 @@
+import { Metadata } from "next";
+import Link from "next/link";
+//TODO:cssインポート
+import styles from "../../page.module.css"
+import { Suspense } from "react";
+import { IAuthentication } from "../../types";
+import { read, readAll } from "../../api";
+import AuthenticationList from "@/app/components/templates/AuthenticationList";
+import AuthenticationCrud from "@/app/components/templates/AuthenticationCrud";
+
+export const metadata: Metadata = {
+    //TODO:タイトル個別画面でmetadata定義
+    title: "authentication/update | remindreact",  
+  };
+  
+
+
+export default async function Page({params}:{params:{id:string}}){
+
+    //restから取得
+    const authentication = await read(params.id);
+
+    return(
+        <>
+            <AuthenticationCrud authentication={authentication}/>
+        </>
+    );
+}  
--- src/app/authentication/create/page.tsx
+++ src/app/authentication/create/page.tsx
@@ -2,8 +2,15 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { IAuthentication } from "../types";
 import AuthenticationCrud from "@/app/components/templates/AuthenticationCrud";
+import { Metadata } from "next";
 
-export default function Page(){
+
+export const metadata: Metadata = {
+    //TODO:タイトル個別画面でmetadata定義
+    title: "authentication/create | remindreact",  
+  };
+
+export default async function Page(){
 
     return(
         <>

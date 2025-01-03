d22632c9c06d6eef8a50d2f5deae3009686b3ae4
waribikimeshi <aee973465@equ.bbiq.jp>
2025-01-03 10:33:13 +0900

    patch

--- src/app/authentication/[id]/update/page.tsx
+++ src/app/authentication/[id]/update/page.tsx
@@ -1,64 +1,166 @@
 
 "use client"  //consoleはF12のブラウザ側
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { IAuthentication } from "../../types";
 import { Read, Update } from "../../api";
 import AuthenticationCrud from "@/app/components/templates/AuthenticationCrud";
 import Head from "next/head";
 import { useParams } from "next/navigation";
+import MyError from "@/app/components/molecules/MyError";
+import MyLoading from "@/app/components/molecules/MyLoading";
+import MyInfo from "@/app/components/molecules/MyInfo";
 
 //クライアントレンダリングでasync awaitはエラーになる
 export default function Page(){
     const params = useParams();
-    const [authentication, setAuthentication] = useState<IAuthentication>();
+    const [authentication, setAuthentication] = useState<IAuthentication | undefined>(undefined);
+    const [error, setError] = useState<string | null>(null); // エラー管理
+    const [info, setInfo] = useState<string | null>(null); // インフォ管理
+    const [isLoading, setIsLoading] = useState<boolean>(false); // ローディング状態
+    const isSubmiting = useRef<boolean>(false); //二度押し防止。再レンダリングされないらしい
+
+    //初期化 reactはstate駆動になってる。DOM操作と書き方違うだけ。だいぶ省略できるね
+    const init = () =>{
+        //constで定義しているだけなので、実際の走行は呼び出し箇所になる。なのでログ実行箇所も想定順にするには注意。
+        console.log('初期化');
+
+        // setAuthentication(undefined); // 前回の結果をクリア。入力値が消えそうなので止める
+
+        setError(null); // エラー状態をリセット
+        setInfo(null); // インフォをリセット
+
+    };
+
+    // データを取得する非同期関数
+    const fetchAuthentication = async () => {
+        try {
+
+            init();
+
+            setIsLoading(true); // ローディング開始
+
+            console.log('データ表示');
+
+            // ReadA関数でデータを取得
+            const result = await Read(params.id as string);
+
+            setAuthentication(result); // データを状態にセット
+            
+        } catch (error: unknown) {
+
+            console.error('fetchで例外');
+
+            if (error instanceof Error) {
+                // `error` が Error インスタンスであれば、エラーメッセージを取得
+                setError(error.message); // 詳細なエラーメッセージを表示
+
+            } else {
+                // `error` がError以外の場合は汎用的なエラーメッセージを表示
+                setError('予期しないエラーが発生しました。');
+            }
+    
+            setAuthentication(undefined); // nullを設定して、表示をクリア
+
+        } finally {
+            setIsLoading(false); // ローディング終了
+        }            
+    };
+
 
     //レンダリングの後に中身が実行される。第二引数は実行するタイミングを指定。空の配列はリロード時のみ実行。変数指定すると変化した際に実行。
     useEffect(() => {
-        console.log("副作用");
+        console.log('useEffect');
 
-        //データ取得関数定義
-        const fetchData = async () => {
-            const result = await Read(params.id as string);
-            setAuthentication(result);
-            console.log(result);
-        };
-
-        //データ取得の実行
-        fetchData();
+        fetchAuthentication();  // データを取得
 
       }, [params.id]);  // URL直変更でもデータを取得。
 
-    console.log("レンダリング");
+    // 再試行ボタンのクリックイベント
+    const handleRetry = () => {
+        console.log('handleRetry');
 
-    if (!authentication) {
-        // データがまだ取得されていない場合、ローディングを表示
-        return <div>Loading...</div>;
-    }
+        init();
+
+        fetchAuthentication(); // データを再取得
+    };    
+
 
       //イベントは各画面から。これは入力値なのでサーバサイドレンダリングできないと思う。
       const handleSubmit = async (authentication:IAuthentication) => {
-        console.log(authentication);
+        if(isSubmiting.current) return; //送信処理中なら抜ける
+        isSubmiting.current = true; //送信処理中
     
-        const newAuthentication = await Update(authentication);
-    
-        //awaitとかいるのか
-        //再描画
-        setAuthentication(newAuthentication);
-    
-        console.log(newAuthentication);
-    
-        // return newAuthentication;
-    
-      };
+        try {
+            console.log("handleSubmit");
 
-    return(
+            init();
+
+            setIsLoading(true); // ローディング開始
+
+            console.log('データ登録');
+
+            // Update関数でデータを登録
+            const newAuthentication = await Update(authentication);
+
+            setAuthentication(newAuthentication); // データを状態にセット
+
+            console.log(newAuthentication);
+
+            //完了メッセージ
+            setInfo("更新されました");
+
+            //登録非活性
+
+        } catch (error: unknown) {
+
+            console.error('fetchで例外');
+
+            if (error instanceof Error) {
+                // `error` が Error インスタンスであれば、エラーメッセージを取得
+                setError(error.message); // 詳細なエラーメッセージを表示
+
+            } else {
+                // `error` がError以外の場合は汎用的なエラーメッセージを表示
+                setError('予期しないエラーが発生しました。');
+            }
+
+            // setAuthentication(undefined); // nullを設定して、表示をクリア。入力消えるのでやめる
+
+        } finally {
+            setIsLoading(false); // ローディング終了
+            isSubmiting.current = false; //送信完了
+        }            
+    
+    };
+
+      console.log("レンダリング");
+
+      return(
         <>
 
             <Head>
                 <title>update | remindreact</title>
-            </Head>        
-            <AuthenticationCrud onSubmit={handleSubmit} defaultValues={authentication}/>
+            </Head>
+            {/* インフォメッセージがある場合の表示 */}
+            {info && (
+                <MyInfo info={info} />
+            )}
+
+            {/* エラーメッセージがある場合の表示 */}
+            {error && (
+                <MyError error={error} handleRetry={handleRetry} />
+            )}
+
+            {/* ローディング状態の表示 */}
+            {isLoading && (
+                <MyLoading />
+            )}
+
+            {/* データがロードされたらリストを表示 */}
+            {!isLoading && (
+                <AuthenticationCrud onSubmit={handleSubmit} defaultValues={authentication}/>
+            )}
 
         </>
     );
